5c34618ffbaf42e150dd7446c18ed398805c05c5
zihua <1477765176@qq.com>
2017-01-09 12:43:27 +0800

    新文件:       Pro7.java

---
+++ Pro7.java
@@ -0,0 +1,25 @@
+public class Pro7 {
+	public static void main(String[]args) {
+		StringBuilder a = new StringBuilder("123456");
+		System.out.println(a.reverse());
+		reverse(-50000);
+	}
+
+	public static int reverse(int x) {
+		if (x == 0)return x;
+		Long tx = Long.valueOf(x);
+		if (x < 0)tx = -tx;
+		StringBuilder a = new StringBuilder(tx.toString());
+		Long t = Long.valueOf(a.reverse().toString());
+		if (t > Integer.MAX_VALUE)return 0;
+		else {
+			Integer xx = Integer.valueOf(x);
+			if (x < 0)xx = -xx;
+			a = new StringBuilder(xx.toString());
+			Integer tt = Integer.valueOf(a.reverse().toString());
+			if (x < 0)return -tt;
+			else return tt;
+		}
+
+	}
+}
