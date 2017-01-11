48795ada2a54872f8994c904910b1a09feb8685a
yahaa <1477765176@qq.com>
2017-01-11 17:33:29 +0800

    java 逆向正则匹配 (暂时写不出)

--- Pro10.java
+++ Pro10.java
@@ -5,7 +5,13 @@ public class Pro10 {
 
 	}
 	public static boolean isMatch(String s, String p) {
-		
+		int lens=s.length();
+		int lenp=p.length();
+		if(!(p.contains(".")||p.contains("*"))&&lenp<lens)return false;
+
+		for(int i=0;i<lens;i++){
+			if(s.charAt(i)!=p.charAt(i))
+		}
 	}
 
 }
