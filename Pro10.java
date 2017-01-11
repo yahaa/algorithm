ae4b210ce72af63b53f818f9afeeab0c5ad93bd0
yahaa <1477765176@qq.com>
2017-01-11 17:04:52 +0800

    新文件:       Pro5.java

---
+++ Pro10.java
@@ -0,0 +1,11 @@
+
+public class Pro10 {
+	public static void main(String[]args) {
+		System.out.println(isMatch("ab","."));
+
+	}
+	public static boolean isMatch(String s, String p) {
+		
+	}
+
+}
---
+++ Pro5.java
@@ -0,0 +1,46 @@
+import static java.lang.Math.*;
+public class Pro5 {
+	public static void main(String[]args) {
+
+		String s = longestPalindrome("abba");
+		System.out.println(s);
+
+	}
+
+	public static String longestPalindrome(String s) {
+		StringBuilder ts = new StringBuilder();
+		ts.append("#");
+		int len = s.length();
+		for (int i = 0; i < len; i++) {
+			ts.append(s.charAt(i));
+			ts.append("#");
+		}
+
+		len = ts.length();
+		int []p = new int[len];
+		int maxRight = 0;
+		int mid = 0;
+
+		int ans = 0;
+		int j = 0;
+		for (int i = 0; i < len; i++) {
+			if (i < maxRight)
+				p[i] = min(p[2 * mid - i], maxRight - i);
+			else
+				p[i] = 1;
+			while (i - p[i] >= 0 && i + p[i] < len && ts.charAt(i - p[i]) == ts.charAt(i + p[i]))p[i]++;
+			if (p[i] + i - 1 < maxRight) {
+				maxRight = p[i] + i - 1;
+				mid = i;
+			}
+			
+			if (p[i] - 1 > ans) {
+				ans = p[i] - 1;
+				j = i;
+			}
+		}
+
+		return s.substring((j - ans) / 2, (j + ans) / 2);
+
+	}
+}
