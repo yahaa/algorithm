7e94eee3196e5b29d8319ab3fe57167572ece00f
zihua <1477765176@qq.com>
2017-01-16 13:46:53 +0800

    新文件:       Pro344.java

---
+++ Pro344.java
@@ -0,0 +1,10 @@
+public class Pro344 {
+	public static void main(String[]args) {
+
+	}
+
+	public String reverseString(String s) {
+		StringBuilder ts=new StringBuilder(s);
+		return ts.reverse().toString();
+	}
+}
