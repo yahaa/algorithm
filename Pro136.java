8bd287d299d8603c317031532047e7a9fb117a05
zihua <1477765176@qq.com>
2017-01-17 13:04:02 +0800

    异或的强大 Pro136.java

---
+++ Pro136.java
@@ -0,0 +1,14 @@
+public class Pro136 {
+	public static void main(String[]args) {
+
+	}
+
+	public int singleNumber(int[] nums) {
+		int len=nums.length;
+		int res=0;
+		for(int i=0;i<n;i++){
+			res^=nums[i];
+		}
+		return res;
+	}
+}
