ec876a51bec1449a0cbc0de772546a497e11b01a
zihua <1477765176@qq.com>
2017-05-03 20:45:59 +0800

    剑指offer 3/66

---
+++ nk/offer/NK3.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import static java.util.Collections.*;
+public class NK3{
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+       ArrayList<Integer> res=new ArrayList<Integer>();
+       while(listNode!=null){
+           res.add(listNode.val);
+           listNode=listNode.next;
+       }
+       reverse(res);
+       return res;
+
+   }
+}
+
+class LIstNode{
+    int val;
+    LIstNode next=null;
+    LIstNode(int va){
+        this.val=val;
+    }
+}
