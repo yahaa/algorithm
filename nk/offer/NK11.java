167d6e9ba2518cd45bda7d2ee2913f3d23b1b74c
zihua <1477765176@qq.com>
2017-05-04 10:11:50 +0800

    求一个数的n次方　n可以为正数负数，n<=63

---
+++ nk/offer/NK11.java
@@ -0,0 +1,27 @@
+public class NK11{
+
+    public double Power(double base, int exponent) {
+        long t = n;
+        double a = x;
+        double result = 1;
+        boolean f = (t > 0) ? true : false;
+        t = Math.abs(t);
+        if (x == 1.0)return 1;
+        if (x == -1 && t % 2 == 0)return 1;
+        if (x == -1 && t % 2 == 1)return -1;
+        if (t >= 63 && !f) return 0;
+        while (t > 0) {
+            if ((t & 1) > 0) {
+                result *= a;
+            }
+            a *= a;
+            t >>= 1;
+        }
+        if (f) {
+            return result;
+        } else {
+            return 1 / result;
+        }
+    }
+
+}
