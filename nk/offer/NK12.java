5b4ef874e726e8185d746763fe8b15d14e3a8108
zihua <1477765176@qq.com>
2017-05-04 10:35:33 +0800

    ArrayList 的使用

---
+++ nk/offer/NK12.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class NK12{
+    public void reOrderArray(int [] array) {
+        ArrayList<Integer>odd=new ArrayList<>();
+        ArrayList<Integer>even=new ArrayList<>();
+        for(int i:array){
+            if(i%2==0)even.add(i);
+            else odd.add(i);
+        }
+        odd.addAll(even);
+        for(int i=0;i<odd.size();i++){
+            array[i]=odd.get(i);
+        }
+
+    }
+}
