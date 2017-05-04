2983f93cd72cac1c31e21c9a3449c796030be8a3
zihua <1477765176@qq.com>
2017-05-05 07:33:32 +0800

    树的镜像递归实现

---
+++ nk/offer/NK17.java
@@ -0,0 +1,10 @@
+public class NK17{
+    public void Mirror(TreeNode root) {
+        if(root==null)return;
+        TreeNode t=root.left;
+        root.left=root.right;
+        root.right=t;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}
