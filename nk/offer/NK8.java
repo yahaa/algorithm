caa5548f53e17edc03e28156d2066db7c12054bd
zihua <1477765176@qq.com>
2017-05-05 11:57:55 +0800

    青蛙条台阶进阶版

--- nk/offer/NK8.java
+++ nk/offer/NK8.java
@@ -1,10 +1,12 @@
 public class NK8{
     public int JumpFloorII(int target) {
+
+        
        //int n=target;
        //if(n==0)return 0;
        //if(n==1)return 1;
        //if(n==2)return 2;
-       //return JumpFloorII(n-1)+JumpFloorII(n-2)+1;
+       //return JumpFloorII(n-1)+JumpFloorII(n-2)+1
        if(target==0)return 0;
        return (int)Math.pow(2,target-1);
    }
