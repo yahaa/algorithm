fda957599d57bacf0a9dcc96c99aeb27acc9096b
zihua <1477765176@qq.com>
2017-05-05 11:56:29 +0800

    两个栈实现队列

--- nk/offer/NK5.java
+++ nk/offer/NK5.java
@@ -1,8 +1,9 @@
 import java.util.Stack;
+
 public class NK5{
     Stack<Integer> stack1 = new Stack<Integer>();
     Stack<Integer> stack2 = new Stack<Integer>();
-
+    
     public void push(int node) {
     	stack1.push(node);
     }
