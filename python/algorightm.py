3f0048570c5c5d34a8e6aeed4f88d081eba1d2da
yzihua <yuanzihua0@gmail.com>
2017-08-03 16:33:34 +0800

    基于数组的堆实现，能够支持最小堆最大堆，堆排序，插入数据，删除堆顶

--- python/algorightm.py
+++ python/algorightm.py
@@ -131,6 +131,113 @@ def se_sort(a):
     return a
 
 
+def binary_search(a, target, start=0, end=-1):
+    """
+    二分差找
+    """
+    if end < 0:
+        end = len(a)
+    left, right = start, end - 1
+    while left <= right:
+        mid = left + (right - left) / 2
+        if target == a[mid]:
+            while mid > 0 and a[mid - 1] == target:
+                mid -= 1
+            return mid
+        elif a[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
+
+
+class Heap(object):
+    """
+    基于list实现的堆,
+    初始化时若指定 minm=True ,为最小堆，反之为最大堆
+    """
+
+    def __init__(self, array=[], size=0, minm=True):
+        self.array = array
+        self.minm = minm
+        if size != 0:
+            self.size = size
+        else:
+            self.size = len(array)
+        self.__create()
+
+    def __create(self):
+        for i in range(0, self.size / 2)[::-1]:
+            self.__siftdown(i)
+
+    def __siftdown(self, index=0):
+        while index * 2 + 1 < self.size:
+            t = index
+            if self.minm:
+                if self.array[index * 2 + 1] < self.array[index]:
+                    t = index * 2 + 1
+                if index * 2 + 2 < self.size and self.array[index * 2 + 2] < self.array[t]:
+                    t = index * 2 + 2
+            else:
+                if self.array[index * 2 + 1] > self.array[index]:
+                    t = index * 2 + 1
+                if index * 2 + 2 < self.size and self.array[index * 2 + 2] > self.array[t]:
+                    t = index * 2 + 2
+
+            if t != index:
+                self.array[index], self.array[t] = self.array[t], self.array[index]
+                index = t
+            else:
+                break
+
+    def __siftup(self, index=-1):
+        if index == -1:
+            index = self.size - 1
+        while index > 0:
+            p = (index - 1) / 2
+            if self.minm:
+                if self.array[index] < self.array[p]:
+                    self.array[index], self.array[p] = self.array[p], self.array[index]
+                else:
+                    break
+            else:
+                if self.array[index] > self.array[p]:
+                    self.array[index], self.array[p] = self.array[p], self.array[index]
+                else:
+                    break
+
+    def pop(self):
+        res = self.top
+        if self.size > 0:
+            self.array[0], self.array[self.size -
+                                      1] = self.array[self.size - 1], self.array[0]
+
+            self.size -= 1
+            self.__siftdown()
+        return res
+
+    @property
+    def top(self):
+        if self.size > 0:
+            return self.array[0]
+        return None
+
+    def setSize(self, val):
+        self.size + val
+
+    def insert(self, val):
+        self.array.append(val)
+        self.size += 1
+        self.__siftup(self.size - 1)
+
+    @staticmethod
+    def heapSort(a, esc=False):
+        heap = Heap(a, minm=esc)
+        while heap.size > 0:
+            heap.pop()
+        return a
+
+
 if __name__ == '__main__':
     print 1654 / 720
     print factorial(6)
@@ -150,3 +257,4 @@ if __name__ == '__main__':
     print one_of_factorial_n(5)
     a = [2, 4, 1, 5, 3, 4, 4, 4, 4, 4, 78]
     print se_sort(a)
+    print binary_search(a, 12)
