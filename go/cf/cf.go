8931bbe4169b4ea1ff2f91b777440be13f81ea85
yzihua <yuanzihua0@gmail.com>
2017-11-23 22:47:37 +0800

    golang 正则表达是使用

--- go/cf/cf.go
+++ go/cf/cf.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+import "regexp"
+import "strings"
+
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -9,7 +12,7 @@ func max(a, b int) int {
 	return b
 }
 
-// codeforce 479A
+// A479 codeforce
 func A479(a, b, c int) int {
 	res := 0
 	res = max(res, a+b+c)
@@ -21,8 +24,26 @@ func A479(a, b, c int) int {
 	return res
 }
 
+// A486 codeforce
+func A486(n int64) int64 {
+	if n%2 == 0 {
+		return n / 2
+	}
+	return (n-1)/2 - n
+
+}
+
+// A208 codeforce
+func A208(s string) string {
+	reg1, _ := regexp.Compile(`(WUB)+`)
+	s = reg1.ReplaceAllString(s, " ")
+	s = strings.TrimLeft(s, " ")
+	s = strings.TrimRight(s, " ")
+	return s
+}
+
 func main() {
-	var a, b, c int
-	fmt.Scanf("%d\n%d\n%d\n", &a, &b, &c)
-	fmt.Println(A479(a, b, c))
+	var s string
+	fmt.Scanf("%s", &s)
+	fmt.Println(A208(s))
 }
